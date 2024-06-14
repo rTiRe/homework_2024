043022c80ce1860d34beef10ce4c3c54cd56068d
Артём Тюрин <zientenin@mail.ru>
2024-06-14 14:46:45 +0300

    Add alert api get tests

---
+++ tests/test_api_get_alert.py
@@ -0,0 +1,12 @@
+import pytest
+from httpx import AsyncClient
+from fastapi import status
+
+
+@pytest.mark.asyncio(scope='session')
+async def test_get_coins(async_client: AsyncClient) -> None:
+    response = await async_client.get('/alerts')
+    assert response.status_code == status.HTTP_200_OK
+    assert 'application/json' in response.headers['content-type']
+    content: dict = response.json()
+    assert content.get('alerts')
